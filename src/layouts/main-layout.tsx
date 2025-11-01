3ccfaa9af7afa2171158a64648a4ae82126c6ba2
Omar Faruk <omarfaruk3393@gmail.com>
2025-11-01 22:20:57 +0600

    chore: remove TopBanner and related logic from MainLayout for cleaner structure

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,26 +1,10 @@
-import TopBanner from "@/components/home/top-banner";
 import Navbar from "@/components/layout/navbar";
-import { useEffect, useState } from "react";
-import { Outlet, useLocation } from "react-router";
+import { Outlet } from "react-router";
 
 export default function MainLayout() {
-  const location = useLocation();
-  const isHome = location.pathname === "/";
-  const [bannerVisible, setBannerVisible] = useState(isHome);
-
-  useEffect(() => {
-    setBannerVisible(location.pathname === "/");
-  }, [location.pathname]);
-
   return (
     <>
-      {isHome && (
-        <TopBanner
-          onVisibilityChange={setBannerVisible}
-          bannerVisible={bannerVisible}
-        />
-      )}
-      <Navbar offsetTop={bannerVisible ? 48 : 0} />
+      <Navbar offsetTop={0} />
       <main className="min-h-screen pt-[66px]">
         <Outlet />
       </main>
